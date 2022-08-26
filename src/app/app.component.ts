b11198ad590a19a8c1b9c176d4861d8e272d6eae
huyluong <mgsh124@gmail.com>
2022-08-25 20:21:54 -0400

    Initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Genders } from './enums/genders';
+import { User } from './interfaces/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'profileEditor';
+  user: User = {
+    name: 'Bob',
+    age: 24,
+    favoriteColor: 'blue',
+    gender: Genders[Genders.Male],
+    active: true
+  };
+  formUser: User = this.user;
+  displayEdit: boolean = false;
+  genders: string[] = [Genders[Genders.Male], Genders[Genders.Female], Genders[Genders.Others]];
+
+  toggleEdit(): void {
+    this.displayEdit = !this.displayEdit;
+  }
+
+  saveChanges(): void {
+    this.user = this.formUser;
+    this.toggleEdit();
+  }
 }
---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,9 @@
+import { Genders } from "../enums/genders";
+
+export interface User {
+    name: string,
+    age: number,
+    favoriteColor: string,
+    gender: string,
+    active: boolean
+}
